a84509b9a89b2b0ad28169a03caea583adce013a
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-05-17 23:16:18 +0200

    Change the image and logo on Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import header_img from "../../public/investment-calculator-logo.png";
+import logo from "../assets/investment-calculator-logo.png";
 
 export default function Header() {
   return (
     <header id="header">
-      <img src={header_img} />
+      <img src={logo} alt="Logo showing a money bag" />
       <h1>Investiment Calculator</h1>
     </header>
   );
