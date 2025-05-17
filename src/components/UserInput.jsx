7b8c265d56d9a6feea84842da24d40ba3f15094f
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-05-17 23:34:37 +0200

    Getting started with a user input component

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,7 +1,28 @@
-import React from "react";
-
 function UserInput() {
-  return <div>UserInput</div>;
+  return (
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label>Initial Investment</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Annual Investment</label>
+          <input type="number" required />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label>Expected Return</label>
+          <input type="number" required />
+        </p>
+        <p>
+          <label>Duration</label>
+          <input type="number" required />
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default UserInput;
