cbf86a50402b7fe2373facce9317ba5bcf7f165d
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2025-05-19 17:28:56 +0200

    Computing values and properly handling number values

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 function App() {
   const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
+    initialInvestment: 15000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
@@ -15,7 +15,7 @@ function App() {
     setUserInput((prevUserInput) => {
       return {
         ...prevUserInput,
-        [inputIdentifier]: newValue,
+        [inputIdentifier]: Number(newValue),
       };
     });
   }
--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,3 +1,9 @@
+import { calculateInvestmentResults } from "../util/investment.js";
+
 export default function Results({ userInput }) {
+  const resultsData = calculateInvestmentResults(userInput);
+
+  console.log(resultsData);
+
   return <div className="center">Results</div>;
 }
